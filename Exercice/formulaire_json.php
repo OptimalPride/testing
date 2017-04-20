1866e6f96d9c8f5d0bfede35c5b580f1a908fdd5
Etudiant <Etudiant>
2017-04-20 17:16:26 +0200

    Exercice 2 sur ajax

--- Exercice/formulaire_json.php
+++ Exercice/formulaire_json.php
@@ -1,8 +1,9 @@
 
 <head><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script></head>
+
 <script>
 	
-$(function(){ 
+$(function(){ //start document ready en Jquery
 
 		var request = $.ajax({ 	
 		  url: "http://jsonplaceholder.typicode.com/users", // cible page requete
@@ -10,7 +11,6 @@ $(function(){
 		});
 		 
 		request.done(function( msg ) { 
-			console.log(msg);
 			var table = "<tr>";
 			$.each(msg[0], function(index, value) {
     			table += "<th>";
@@ -23,7 +23,16 @@ $(function(){
 
 			    	$.each(msg[i], function(index, value) {
 				    	table += "<td>";
-			    	if(index == "address") {
+				    if (index == "name") {
+				    	table += "<a href='";
+				    	table += value;
+				    	table += "' name='";
+				    	table += i;
+				    	table += "'>";
+				    	table += value;
+				    	table += "</a>";
+				    }
+			    	else if(index == "address") {
 						table += value.street;
 						table += " ";
 						table += value.city;
@@ -43,16 +52,73 @@ $(function(){
 			}; 
 			table += "</tr>";
 			$( "#tableau" ).html( table );
-			console.log(table);
+
+			$("a").click(function(e) { // traitement informations individuelle
+				e.preventDefault(); 
+				var	name = $(this).attr('href');
+				var	i = $(this).attr('name');
+				var info = name;
+				info += " : ";
+				info += "<table border='1'>";
+				info += "<tr>";
+				$.each(msg[i], function(index, value) {
+				    if (index == "name") {
+				    }
+				    else {
+	    			info += "<th>";
+	    			info += index;
+	    			info += "</th>";
+	    			}
+				});
+				info += "</tr>";
+			    info += "<tr>";
+				$.each(msg[i], function(index, value) {
+				    if (index == "name") {
+				    }
+			    	else if(index == "address") {
+			    		info += "<td>";
+						info += value.street;
+						info += " ";
+						info += value.city;
+						info += " ";
+						info += value.zipcode;
+						info += "</td>";
+			    	}
+			    	else if(index == "company") {
+			    		info += "<td>";
+						info += value.name;	
+						info += "</td>";
+			    	}
+			    	else {
+			    		info += "<td>";
+						info += value;	
+						info += "</td>";	    		
+			    	} 
+					}); 
+			    info += "</tr>";
+	    		info += "</table>";
+				$( "#info" ).html( info );
+			});
 		});
 
-		request.fail(functi
-			on(XPDDR, data){
+		request.fail(function(XPDDR, data){
 			alert("Request fail !")
 		})
-});
+
+
+//testing shit
+var test = "abc def";
+var result = test.split(' ');
+console.log (result);
+
+}); //fin function()
 </script>
 
 <table border="1" id="tableau">
 
 </table>
+
+<br><br>
+<div id="info"></div>
+
+<div id="test"></div>
